12a9510f01130151c74bd3f4e7797b87e0652610
Daniel Schmid <danieljschmid@gmail.com>
2018-10-13 16:25:16 +0100

    highlight text and jump to sentance

--- Interactive Video/js/main.js
+++ Interactive Video/js/main.js
@@ -1,16 +1,30 @@
 
 //Get the video Element
-const vid = document.getElementsByTagName('video');
+const vid = document.getElementsByTagName('video')[0];
 
 // Select Span Elements
-const span = document.getElementsByTagName('span')
+const span = document.querySelectorAll('span');
 
-//Add time Update Event Listener
+
+
+//highlight Text
 vid.ontimeupdate = () => {
   const vidTime = vid.currentTime;
   for (let i = 0; i < span.length; i += 1 ) {
-    if(vidTime > span[i].getAttrbute('data-start') && vidTime < span[i].getAttrbute('data-end') ) {
-      span[i].style.color = '#e8743a'
+    if(vidTime > span[i].getAttribute('data-start') && vidTime < span[i].getAttribute('data-end') ) {
+      span[i].style.color = '#e8743a';
+    } else {
+      span[i].style.color = '';
     }
   }
+};
+
+// jump to selected sentance in Video
+
+for ( i = 0; i < span.length; i ++) {
+  span[i].addEventListener('click', (event) => {
+    vid.currentTime = event.target.getAttribute('data-start');
+      vid.play();
+
+  });
 }
